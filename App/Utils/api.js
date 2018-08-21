9a265ee50933c1978e6e59aa34adaa9c66379a96
jferreira93 <jferreira@loopstud.io>
2018-08-21 10:17:35 -0300

    Implement connection to Firebase

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -9,17 +9,17 @@ export default {
     const url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
   },
-  getNotes(username){
+  getNotes(username) {
     username = username.toLowerCase().trim();
-    const url = `https://github-saver.firebaseio.com/${username}.json`;
+    const url = `https://githubnotetaker-juan.firebaseio.com//${username}.json`;
     return fetch(url).then((res) => res.json());
   },
-  addNote(username, note){
+  addNote(username, note) {
     username = username.toLowerCase().trim();
-    const url = `https://github-saver.firebaseio.com/${username}.json`;
+    const url = `https://githubnotetaker-juan.firebaseio.com//${username}.json`;
     return fetch(url, {
       method: 'post',
       body: JSON.stringify(note)
     }).then((res) => res.json());
-  }
+}
 };
